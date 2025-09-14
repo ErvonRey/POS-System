48791e68b32afb7c9148191bc49777b604555e44
Ervon Rey <ervonreyqmellisa@gmail.com>
2025-09-14 14:23:17 +0800

    Add files via upload

--- POS_System/src/Connection/DBConnection.java
+++ POS_System/src/Connection/DBConnection.java
@@ -8,29 +8,32 @@ import java.sql.SQLException;
 
 public class DBConnection {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/";
-    private static final String USER = "zumi";
-    private static final String PASSWORD = "mellisaAdmin1023!";
+    private static final String URL = "jdbc:mysql://localhost:3306/";   //URL of the Database
+    private static final String USER = "zumi";                          //USERNAME of the Database
+    private static final String PASSWORD = "mellisaAdmin1023!";         //PASSWORD of the Database
     
     /*
-    This is just a practice on my local device, thus I have ignored the fact that I had put
-    Sensitive information in this class file.
+    
+    This is just a practice on my local device,
+    thus I have ignored the fact that I had put
+    Sensitive information in this class.
+    
     */
     
+    public static void main(String[] args) {
+        DBConnection.getConnection();
+    }
+    
     public static Connection getConnection(){
-        Connection conn = null;
+        Connection connection = null;
         try {
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//            System.out.println("Connected Successfully");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//            System.out.println("Connected Successfully");     //debugging
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("Failed Connecting to Database!");
+            System.out.println("Error at connecting to Database");
         }
-        return conn;
+        return connection;
     }
-
-//    public static void main(String[] args) {
-//        Connection conn = getConnection();
-//    }
     
 }
--- POS_System/src/pos_system/POS_System.java
+++ POS_System/src/pos_system/POS_System.java
@@ -5,12 +5,11 @@ package pos_system;
  * @author Mellisa
  */
 public class POS_System {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
     
 }
